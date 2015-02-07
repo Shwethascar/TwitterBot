be8309eea864960dbe23b97a86ebe8c4bf50c620
Shwethascar <shwethagopalan@Shwethas-MBP.home>
2015-02-06 20:15:06 -0500

    Initial Commit

---
+++ TwitterBot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterBot
+//
+//  Created by Shwetha Gopalan on 2/6/15.
+//  Copyright (c) 2015 Shwe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterBot/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TwitterBot
+//
+//  Created by Shwetha Gopalan on 2/6/15.
+//  Copyright (c) 2015 Shwe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
